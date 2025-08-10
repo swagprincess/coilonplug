71b927f0b144e2d0188b23528f72a7115994bab7
lucy <lucyhapsti@gmail.com>
2025-08-10 21:54:02 +0200

    first complete code to test + makefile

---
+++ main.c
@@ -0,0 +1,87 @@
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+
+volatile uint16_t delayBeforeHigh = 125; //ticks at 8mhz prescaler 256, 4ms base pulse
+volatile uint16_t startTime14 = 0;
+volatile uint16_t startTime23 = 0;
+volatile bool lastStatePB0 = false;
+volatile bool lastStatePB1 = false;
+volatile bool set23Low = false;
+volatile bool set14Low = false;
+
+
+ISR(PCINT0_vect){ // interrupt for all port b pins
+
+    if ((PINB & (1 << PB0)) & !lastStatePB0) {   //IGN 2 & 3
+        // PB0 is HIGH 
+        startTime23 = TCNT1; // set timer timestamp
+        set23Low = true; // mark pin as has to go low eventually 
+        lastStatePB0 = true;
+
+    } else if (lastStatePB0){
+        // PB0 is LOW, turn off PA1 (2 & 3)
+        PORTA |= (1 << PA1); // set high for off
+
+        delayBeforeHigh = TCNT1 - startTime23 - 75; //2.4ms setting
+
+        lastStatePB0 = false;
+    }
+
+    if ((PINB & (1 << PB1)) & !lastStatePB1) {   //IGN 1 & 4
+        // PB1 is HIGH
+        startTime14 = TCNT1; // set timer timestamp
+        set14Low = true; // mark pin as has to go low eventually 
+
+        lastStatePB1 = true;
+
+    } else if (lastStatePB1){
+        // PB1 is LOW, turn off PA2 (1 & 4)
+        PORTA |= (1 << PA2); // set high for off
+
+        delayBeforeHigh = TCNT1 - startTime14 - 75; //2.4ms setting
+
+        lastStatePB1 = false;
+
+    }
+     
+}
+
+
+int main() {
+
+    //set output ports as output and set high
+    DDRA |= ((1 << DDA1) | (1<< DDA2));
+    PORTA |= ((1 << PA1) | (1 << PA2));
+
+    //set input ports as input
+    DDRB &= ~((1 << DDB0) | (1 << DDB1));
+
+    TCCR1B = 0; // stop timer
+
+    TCCR1B |= (1 << CS12); // start timer at prescaler 256
+
+    sei(); //enable interrupts
+
+
+    while (1){
+        if (set23Low){
+            if (startTime23 + delayBeforeHigh <= TCNT1){
+                PORTA &= ~(1 << PA1); // set low to start trigger
+                set23Low = false;
+            }
+        }
+        if (set14Low){
+            if (startTime14 + delayBeforeHigh <= TCNT1){
+                PORTA &= ~(1 << PA2); // set low to start trigger
+                set14Low = false;
+            }
+        }
+    }
+
+
+
+}
